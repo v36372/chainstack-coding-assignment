8667a1d32586c4c3431c241580c324924d9b9263
Tin Nguyen <nguyentrongtin2331@gmail.com>
2018-09-05 21:42:50 +0700

    Add db function to create user and quota

--- repo/users.go
+++ repo/users.go
@@ -20,6 +20,7 @@ func init() {
 type IUser interface {
 	GetByEmail(email string) (*models.User, error)
 	List(nextId, limit int) ([]models.User, error)
+	CreateUserAndQuota(*models.User, *models.UserQuota) error
 	Create(*models.User) error
 	Update(*models.User) error
 	Delete(*models.User) error
@@ -35,6 +36,33 @@ func (u user) Update(user *models.User) error {
 
 func (u user) Delete(user *models.User) error {
 	return u.delete(user)
+}
+
+func (u user) CreateUserAndQuota(user *models.User, quota *models.UserQuota) (err error) {
+	if quota == nil {
+		return u.create(user)
+	}
+	tx := infra.PostgreSql.Begin()
+	err = tx.Create(user).Error
+	if err != nil {
+		tx.Rollback()
+		return
+	}
+
+	quota.UserId = user.Id
+	err = tx.Create(quota).Error
+	if err != nil {
+		tx.Rollback()
+		return
+	}
+
+	err = tx.Commit().Error
+	if err != nil {
+		tx.Rollback()
+		return
+	}
+
+	return nil
 }
 
 func (user) List(nextId, limit int) (users []models.User, err error) {
