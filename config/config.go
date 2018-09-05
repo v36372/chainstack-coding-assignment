9ae68f5e0ae854facb5600046418607410bacc73
Tin Nguyen <nguyentrongtin2331@gmail.com>
2018-09-05 14:05:10 +0700

    Create basic config

---
+++ config/config.go
@@ -0,0 +1,47 @@
+package config
+
+import (
+	"chainstack/cmd"
+	"fmt"
+	"sync"
+)
+
+var (
+	conf Config
+	once sync.Once
+)
+
+type Config struct {
+	App App
+}
+
+type App struct {
+	Host string
+	Port int
+}
+
+func init() {
+	// Init CLI commands
+	cmd.Root().Use = "bin/chainstack --config <Config path>"
+	cmd.Root().Short = "chainstack - Provide API for chainstack"
+	cmd.Root().Long = "chainstack"
+
+	cmd.SetRunFunc(load)
+}
+
+func load() {
+	once.Do(func() {
+		if err := cmd.GetViper().Unmarshal(&conf); err != nil {
+			fmt.Println("load viper fail")
+		}
+	})
+}
+
+func Load() {
+	load()
+}
+
+func Get() Config {
+	load()
+	return conf
+}
