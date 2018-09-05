57679f11909cf00ab183378748a38943fa861fd3
Tin Nguyen <nguyentrongtin2331@gmail.com>
2018-09-05 17:34:38 +0700

    Response message in JSON

--- utilities/uer/handle.go
+++ utilities/uer/handle.go
@@ -1,22 +1,15 @@
 package uer
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
 )
 
 func HandleErrorGin(e error, c *gin.Context) {
 	err := ToStatusError(e)
-	switch err.Status {
-	case http.StatusNotFound, http.StatusUnauthorized, http.StatusForbidden:
-	case http.StatusInternalServerError:
-		c.AbortWithError(err.Status, e)
-	case http.StatusBadRequest:
-		c.AbortWithError(err.Status, err.Err)
-		return
-	}
-	c.AbortWithStatus(err.Status)
+	c.JSON(err.Status, gin.H{
+		"msg": err.Message,
+	})
+	c.Abort()
 }
 
 func HandleNotFound(c *gin.Context) {
