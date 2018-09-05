62336d3543eb6ae611caefe444eecc8455f70442
Tin Nguyen <nguyentrongtin2331@gmail.com>
2018-09-05 18:56:09 +0700

    Add get resource handler

--- app/handler/user.go
+++ app/handler/user.go
@@ -3,14 +3,19 @@ package handler
 import (
 	"chainstack/app/entity"
 	"chainstack/app/form"
+	"chainstack/app/params"
+	"chainstack/app/view"
 	"chainstack/middleware"
 	"chainstack/utilities/uer"
+	"fmt"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 )
 
 type userHandler struct {
 	user      entity.User
+	resource  entity.Resource
 	secCookie *middleware.SecCookie
 }
 
@@ -35,5 +40,66 @@ func (h userHandler) Login(c *gin.Context) {
 		return
 	}
 
+	c.Status(200)
+}
+
+func getPureRequestURI(URI string, prefix string) string {
+	firstIndex := strings.IndexRune(URI, '?')
+	if firstIndex != -1 {
+		return URI[len(prefix):firstIndex]
+	}
+	return URI[len(prefix):]
+}
+
+func (h userHandler) ListResources(c *gin.Context) {
+	currentUser := middleware.Auth.GetCurrentUser(c)
+	if currentUser == nil {
+		uer.HandleUnauthorized(c)
+		return
+	}
+
+	userId := params.GetUserIdUrlParam(c)
+	if userId == 0 {
+		userId = currentUser.Id
+	}
+
+	if userId != currentUser.Id && currentUser.IsAdmin == false {
+		uer.HandlePermissionDenied(c)
+		return
+	}
+
+	nextId, limit := params.GetNextIdGetResourcesParam(c)
+
+	resources, err := h.resource.GetByUserId(userId, nextId, limit)
+	if err != nil {
+		uer.HandleErrorGin(err, c)
+		return
+	}
+
+	var pagination *view.Pagination
+	resourceViews := view.NewResources(resources)
+	if len(resources) == limit {
+		url := c.Request.RequestURI
+		if strings.Index(url, "/") == 0 {
+			url = url[1:]
+		}
+		version := c.Param("version")
+		// NextUrl
+		nextUrl := fmt.Sprintf("%s?%s=%d",
+			getPureRequestURI(url, version),
+			params.ParamUrlNextId,
+			resources[len(resources)-1].Id)
+		pagination = &view.Pagination{
+			NextUrl: nextUrl,
+		}
+	}
+	view.ResponseOKWithPagination(c, resourceViews, pagination)
+}
+
+func (h userHandler) CreateResource(c *gin.Context) {
+	c.Status(200)
+}
+
+func (h userHandler) DeleteResource(c *gin.Context) {
 	c.Status(200)
 }
