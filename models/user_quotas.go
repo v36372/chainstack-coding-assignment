bd4c9a0e4bbe615e7fb561e81d1014e288cf2867
Tin Nguyen <nguyentrongtin2331@gmail.com>
2018-09-06 14:35:40 +0700

    Add field CurrentQuotaLeft to model user quota

--- models/user_quotas.go
+++ models/user_quotas.go
@@ -3,13 +3,14 @@ package models
 import "time"
 
 type UserQuota struct {
-	Id        int
-	UserId    int
-	Quota     int
-	CreatedAt time.Time
-	UpdatedAt time.Time
-	UpdatedBy int
-	CreatedBy int
+	Id               int
+	UserId           int
+	Quota            int
+	CurrentQuotaLeft int
+	CreatedAt        time.Time
+	UpdatedAt        time.Time
+	UpdatedBy        int
+	CreatedBy        int
 }
 
 func (UserQuota) TableName() string {
