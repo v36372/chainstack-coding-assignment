b81f78b7f0ac6a31e0ad5fde331d7bc2594a5852
Tin Nguyen <nguyentrongtin2331@gmail.com>
2018-09-06 14:37:11 +0700

    Add validate quota is non-negative

--- app/form/quota.go
+++ app/form/quota.go
@@ -2,12 +2,13 @@ package form
 
 import (
 	"chainstack/utilities/uer"
+	"errors"
 
 	"github.com/gin-gonic/gin"
 )
 
 type UserQuota struct {
-	Quota int `form:"quota" json:"quota""`
+	Quota int `form:"quota" json:"quota"`
 }
 
 func (userQuota *UserQuota) FromCtx(c *gin.Context) error {
@@ -15,5 +16,9 @@ func (userQuota *UserQuota) FromCtx(c *gin.Context) error {
 		return uer.BadRequestError(err)
 	}
 
+	if userQuota.Quota < 0 {
+		err := uer.BadRequestError(errors.New("Quota should not be negative"))
+		return err
+	}
 	return nil
 }
